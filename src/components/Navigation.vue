23caf617343dcd620cdd0f155db25c31144a48cd
Clemens Dehnert <clemensdehnert@gmail.com>
2024-08-03 21:45:41 +0200

    modal working

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -26,12 +26,31 @@
           <router-link to="/settings" class="nav-link" active-class="active">Settings</router-link>
         </li>
         <li class="nav-item">
-          <button class="btn btn-outline-primary mx-2" type="submit">Login</button>
+          <button class="btn btn-outline-primary mx-2" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Login</button>
         </li>
       </ul>
     </div>
   </nav>
-  <AuthModal ref="thisModal" />
+
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script lang="ts">
