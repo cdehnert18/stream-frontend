d41919a0c74a5eda19bb4839c658c5174b41f2b3
Clemens Dehnert <clemensdehnert@gmail.com>
2024-08-03 21:49:59 +0200

    modal in own file

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import ModalComponent from './ModalComponent.vue'
+</script>
+
 <template>
   <nav class="navbar navbar-expand w-100">
     <div class="container-fluid">
@@ -26,35 +30,25 @@
           <router-link to="/settings" class="nav-link" active-class="active">Settings</router-link>
         </li>
         <li class="nav-item">
-          <button class="btn btn-outline-primary mx-2" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Login</button>
+          <button
+            class="btn btn-outline-primary mx-2"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            Login
+          </button>
         </li>
       </ul>
     </div>
   </nav>
-
-  <!-- Modal -->
-  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Understood</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
+  <ModalComponent />
 </template>
 
 <script lang="ts">
 export default {
-  name: 'NavigationComponent'
+  name: 'NavigationComponent',
+  components: {
+    ModalComponent, // Registriere die Modal-Komponente
+  },
 }
 </script>
